a6e4ddf81558e9252587595e4eccc257a9517ae3
Milton Tom <miltontom16@gmail.com>
2024-05-27 06:29:43 +0530

    feat: make packages in the user's home directory

--- lovecraft.py
+++ lovecraft.py
@@ -8,9 +8,10 @@ from sys import exit
 import click
 
 
-VERSION = '0.1.0'
+VERSION = '0.2.0'
 CONFIG_FILE_NAME = 'craft.ini'
 EXCLUSIONS_FILE_NAME = '.exclude'
+CRAFTS_DIR = join(os.getenv('USERPROFILE'), 'crafts')
 EXIT_SUCCESS = 0
 EXIT_FAILURE = 1
 
@@ -43,7 +44,7 @@ def zip_file(folder_path, output_path, exclude=[]):
             for dir_to_exclude in dirs_to_exclude:
                 dirs.remove(dir_to_exclude)
 
-            files = [f for f in files if f != EXCLUSIONS_FILE_NAME and f != CONFIG_FILE_NAME]
+            files = [f for f in files if f not in exclude]
                 
             for file in files:
                 file_path = os.path.join(root, file)
@@ -51,11 +52,9 @@ def zip_file(folder_path, output_path, exclude=[]):
                     zipf.write(file_path, os.path.relpath(file_path, folder_path))
 
 
-def parse_exclusions(game_source, game_destination, game_name):
-    excluded_list = []
-
-    if game_name in os.listdir(game_source):
-        excluded_list.append(game_destination)
+def parse_exclusions(game_source):
+    # files to be excluded by default
+    excluded_list = [EXCLUSIONS_FILE_NAME, CONFIG_FILE_NAME]
 
     if EXCLUSIONS_FILE_NAME not in os.listdir(game_source):
         return excluded_list
@@ -72,7 +71,7 @@ def parse_exclusions(game_source, game_destination, game_name):
 def package_game(config):
     game_name = config['name']
     game_source = config['source']
-    game_destination = config['destination']
+    game_destination = join(CRAFTS_DIR, game_name)
     game_icon = config['icon']
 
     love_exe = shutil.which('love')
@@ -80,14 +79,21 @@ def package_game(config):
         print('Error: love.exe is not in PATH')
         return EXIT_FAILURE
 
+    if not os.path.exists(CRAFTS_DIR):
+        os.mkdir(CRAFTS_DIR)
+    
+    # paths are case insensitive in Windows
+    if os.path.exists(game_destination):
+        shutil.rmtree(game_destination)
+
     if not os.path.exists(game_destination):
         os.mkdir(game_destination)
 
     archive_name = f'{join(game_destination, game_name)}.love'
-    excluded_list = parse_exclusions(game_source, game_destination, game_name)
+    excluded_list = parse_exclusions(game_source)
     zip_file(game_source, archive_name, excluded_list)
 
-    exe_name = f'{join(game_destination, game_name)}.exe'
+    exe_name = f'{join(game_destination, game_name.lower())}.exe'
     os.system(f'copy /b "{love_exe}" + {archive_name} {exe_name} > NUL')
     os.remove(archive_name)
 
@@ -104,34 +110,28 @@ def package_game(config):
 
 
 def parse_config(src):
+    fallbacks = {
+        'source': src,
+        'name': os.path.basename(src),
+        'icon': None
+    }
+
     if CONFIG_FILE_NAME not in os.listdir(src): 
         print('No config file found... Using fallback values')
-        src_basename = os.path.basename(src)
-        return {
-            'source': src,
-            'name': src_basename,
-            'destination': join(src, src_basename),
-            'icon': None
-        }
+        return fallbacks
 
     parser = configparser.ConfigParser()
     parser.read(join(src, CONFIG_FILE_NAME))
 
-    name = parser.get('Game', 'name', fallback=os.path.basename(src)).lower()
-    destination = (join(
-        parser.get('Game', 'destination', fallback=src),
-        name))
-    if not isabs(destination):
-        destination = join(src, destination)
-    icon = parser.get('Game', 'icon', fallback=None)
+    name = parser.get('Game', 'name', fallback=fallbacks['name'])
+    icon = parser.get('Game', 'icon', fallback=fallbacks['icon'])
     if icon and not isabs(icon):
         icon = join(src, icon)
 
     return {
         'source': src,
         'name': name,
-        'destination': normpath(destination),
-        'icon': normpath(icon) if icon else icon
+        'icon': normpath(icon) if icon else None
     }
 
 
@@ -141,25 +141,35 @@ def is_love2d_project(src):
 
 
 @click.command()
+@click.option('--name', '-n', help='Name for the game.')
+@click.option('--icon', '-i', help='Path to \'.ico\' file.', type=click.Path())
+@click.option('--crafts-dir', is_flag = True, help='Show the packaging location and exit.')
 @click.version_option(VERSION)
 @click.argument('src', default='.')
-def main(src):
+def main(src, name, icon, crafts_dir):
     '''
     Packages LÖVE game for distribution.
 
     SRC - Path to project directory
     '''
     try:
+        if crafts_dir:
+            print(CRAFTS_DIR)
+            exit(EXIT_SUCCESS)
+
         src = os.path.abspath(src)
         if not is_love2d_project(src):
             print('Error: Not a LÖVE game project directory')
             exit(EXIT_FAILURE)
-        
-        config = parse_config(src)
-        exit_status = package_game(config)
 
+        if name or icon:
+            config = {'source': src, 'name': name, 'icon': icon}
+        else:
+            config = parse_config(src)
+
+        exit_status = package_game(config)
         if exit_status == EXIT_SUCCESS:
-            print(f'\nPackage successfully created at \'{os.path.abspath(config['destination'])}\'')
+            print(f'Package successfully created at \'{join(CRAFTS_DIR, config['name'])}\'')
     except FileNotFoundError as e:
         print(e)
     except Exception as e:
